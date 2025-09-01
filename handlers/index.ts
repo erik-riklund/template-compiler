d94f64b82d58a878f6c2039a1f5ea78006d946ac
Erik R <erik.riklund@outlook.com>
2025-09-01 14:49:34 +0200

    removed handlers: include, render

--- handlers/index.ts
+++ handlers/index.ts
@@ -8,8 +8,6 @@ import type { Handler } from 'types'
 import { handler as blockEnd } from './blocks/end'
 import { handler as elseBlock } from './blocks/else'
 import { handler as eachBlock } from './blocks/each'
-import { handler as includeBlock } from './blocks/include'
-import { handler as renderBlock } from './blocks/render'
 import { handler as withBlock } from './blocks/with'
 import { handler as whenBlock } from './blocks/when'
 import { handler as variable } from './variables'
@@ -22,8 +20,6 @@ export const getHandlers = (): Array<Handler> =>
     blockEnd,
     elseBlock,
     eachBlock,
-    includeBlock,
-    renderBlock,
     withBlock,
     whenBlock,
     variable
